af55a6d99164eb0693bacd8b38d5bee4601fcd3a
Wayne Edwards <waedwards42@yahoo.com>
2021-03-13 19:58:59 -0800

    update main and modals in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,7 @@ var loadTasks = function() {
 var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+
 $(".list-group").on("click", "p", function() {
     var text = $(this)
         .text()
